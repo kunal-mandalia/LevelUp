6f70513ca0b143ec222400bad0a2acd140c5e4ee
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2016-03-14 18:35:20 +0000

    removed all and added with git ignore

--- app/config/environmentVariables.js
+++
@@ -1,6 +0,0 @@
-var envVars = {};
-//process.env variables set from Heroku
-envVars.db = process.env.MONGOLAB_URI || "mongodb://localhost/levelup";
-envVars.port = process.env.PORT || 3000;
-
-module.exports = envVars;
--- config/environmentVariables.js
+++
@@ -1,6 +0,0 @@
-var envVars = {};
-
-envVars.db = process.env.MONGOLAB_URI || "mongodb://localhost/levelup";
-envVars.port =  process.env.PORT || 3000;
-
-module.exports = envVars;
